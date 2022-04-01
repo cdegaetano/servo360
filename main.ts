7bf531128f7aa3292ddcf259defd89c7b26a9d13
cdegaetano <cdegaetano@florence.k12.nj.us>
2022-04-01 13:10:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     if (Rotate == 0) {
-        for (let index = 0; index < 100; index++) {
-            servos.P0.run(50)
-            servos.P1.run(50)
-        }
-        servos.P0.run(0)
-        servos.P1.run(0)
+        servos.P0.run(50)
+        servos.P1.run(50)
         Rotate += 1
     } else {
+        servos.P0.run(0)
         servos.P1.run(0)
         Rotate += -1
     }
